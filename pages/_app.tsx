4d4c736629d010584e9e491098d8f19ed785ede1
Mark James <markjames@theambassadors.com>
2023-01-20 10:04:40 +0000

    Fix title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,8 +15,8 @@ import '../public/globals.css'
 import type { AppProps } from 'next/app'
 import type { MarkdocNextJsPageProps } from '@markdoc/next.js'
 
-const TITLE = 'Markdoc';
-const DESCRIPTION = 'A powerful, flexible, Markdown-based authoring framework';
+const TITLE = 'ATG Code Guidelines';
+const DESCRIPTION = 'Hints and tips on code style and conventions';
 
 function collectHeadings(node, sections = []) {
   if (node) {
